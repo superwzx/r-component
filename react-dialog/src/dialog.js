8d03f3b454de91c9ded96e1483662b8cc65d5328
f2e <f2e@f2edeiMac.local>
2016-08-16 18:14:00 +0800

    dialog update

--- react-dialog/src/dialog.js
+++ react-dialog/src/dialog.js
@@ -1,15 +1,24 @@
 
 let React = require('react');
 
-let DialogHead = require('./DialogHead');
-
-let DialogBody = require('./DialogBody');
-
-let DialogFoot = require('./DialogFoot');
-
 let Dialog = React.createClass({
-	getInitialState () {
 
+	getInitialState () {
+		return {
+			title: config.title,
+			content: config.content,
+			buttons: [{
+				short: 'OK',
+				name: '确定',
+				className: '',
+				handle: (e) => {}
+			}, {
+				short: 'CANCEL',
+				name: '取消',
+				className: '',
+				handle: (e) => {}
+			}]
+		}
 	},
 
 	componentWillMount () {
@@ -17,8 +26,53 @@ let Dialog = React.createClass({
 	},
 
 	render () {
+
+		let title = (
+			<div>
+				<div className="dialog-title">
+					<div className="dialog-title-inner">
+						{this.state.title}
+					</div>
+				</div>
+			</div>
+		);
+
+		let content = (
+			<div className="dialog-content">
+				<div className="dialog-content-inner">
+					{this.state.content}
+				</div>
+			</div>
+		);
+
+		let buttons = (
+			<div className="dialog-buttons">
+				<div className="dialog-buttons-inner">
+					{
+						this.state.buttons.map(function (item) {
+							return (
+								<button
+									onClick={item.handle}
+								>
+									item.name
+								</button>
+							)
+						})
+					}
+				</div>
+			</div>
+		);
+
 		return (
-			
+			<div className="dialog-mask">
+				<div className="dialog">
+					<div className="dialog-inner">
+						{title}
+						{content}
+						{buttons}
+					</div>
+				</div>
+			</div>
 		)
 	}
 });
