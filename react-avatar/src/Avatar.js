41988a5ac022690cf907e369e17810bd0817502c
H-Yvonne <497829788@qq.com>
2016-08-18 10:09:59 +0800

    modify avatar

--- react-avatar/src/Avatar.js
+++ react-avatar/src/Avatar.js
@@ -4,21 +4,41 @@ var Avatar = React.createClass({
     componentWillMount: function () {
         this.style = {
             root: {
+                display: 'table',
                 width: this.props.size || 40,
                 height: this.props.size || 40,
                 textAlign: 'center',
                 borderRadius: '50%',
+                background: this.props.background || 'none',
                 overflow: 'hidden'
+            },
+            inner: {
+                display: 'table-cell',
+                textAlign: 'center',
+                verticalAlign: 'middle'
+            },
+            icon: {
+                fontSize: this.props.size*0.5
             }
         }
         this.src = this.props.src || this.props.defaultSrc;
     },
     render: function () {
-        return (
-            <div style={this.style.root}>
-                <img src={this.src} width="100%" height="100%" />
-            </div>
-        )
+        if(this.props.icon) {
+            return (
+                <div style={this.style.root}>
+                    <div style={this.style.inner}>
+                        <span className={this.props.icon} style={this.style.icon}></span>
+                    </div>
+                </div> 
+            )
+        } else {
+            return (
+                <div style={this.style.root}>
+                    <img src={this.src} width="100%" height="100%" />
+                </div>
+            )
+        }
     }
 });
 
