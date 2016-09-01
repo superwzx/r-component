cf297d8fd6050049306f7746382f41ac8639f5eb
wzx <wzx175@gmail.com>
2016-09-01 16:11:04 +0800

    update

--- react-dialog/src/index.js
+++ react-dialog/src/index.js
@@ -1,23 +1,19 @@
 import React, {Component, PropTypes} from 'react';
-import ReactDom from 'react-dom';
+import ReactDOM from 'react-dom';
 import Dialog from './Dialog';
 import Button from '../../react-button/src/Button';
 
+/**
+ *
+ * @param txt
+ */
 const reactAlert = txt => {
-
 	let buttons = [
-		<Button
-			size="lg"
-		>
-			Ok
-		</Button>
+		<Button size="lg">Ok</Button>
 	];
-
 	let overlay = document.createElement('div');
-
 	document.body.appendChild(overlay);
-
-	ReactDom.render(
+	ReactDOM.render(
 		<Dialog
 			buttons={buttons}
 		>
@@ -28,4 +24,46 @@ const reactAlert = txt => {
 };
 
 
-export default reactAlert;
+/**
+ *
+ * @param config
+ */
+const reactConfirm = (config) => {
+	let buttons = [
+		<Button size="log">Ok</Button>,
+		<Button size="log">Cancel</Button>
+	];
+	let overlay = document.createElement('div');
+	document.body.appendChild(overlay);
+	ReactDOM.render(
+		<Dialog
+			{buttons}
+		>
+			{config.content}
+		</Dialog>
+	);
+};
+
+
+/**
+ *
+ * @param config
+ */
+const reactModal = (config) => {
+	let buttons = Object.assign([
+		<Button size="log">Ok</Button>,
+		<Button size="log">Cancel</Button>
+	], config.buttons);
+	let overlay = document.createElement('div');
+	document.body.appendChild(overlay);
+	ReactDOM.render(
+		<Dialog
+			{buttons}
+		>
+			{config.content}
+		</Dialog>
+	);
+};
+
+
+export default {reactAlert, reactConfirm, reactModal};
